70c08e5f0ba43bef05c0e6c1e3035edf4f677a09
Dovganyuk <asdovganyuk@gmail.com>
2021-08-17 15:22:17 +0300

    finished 3rd sprint, check point before 3rd project

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -7,6 +7,6 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('group/<slug:slug>/', views.group_posts),
+    path('group/<slug:slug>/', views.group_posts, name='slug'),
 
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Интересная главная страница')
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context) 
 
 
-def group_posts(request, slug):
-    return HttpResponse(f'{slug}? GODDAM RIGHT!!')
+def group_posts(request):
+    template = 'posts/group_list.html'
+    description = 'Это главная страница проекта Yatube'
+    context = {
+        'description': description,
+    }
+    return render(request, template, context) 
